a793758055b03bf1d56f5c59588cb0afed461dde
Fatima Irshad <154042550+FatimaIrshad123@users.noreply.github.com>
2024-02-19 09:53:56 +0500

    Update middleware.js

--- backened/middleware.js
+++ backened/middleware.js
@@ -4,9 +4,7 @@ const { jwtSecret } = require('./config')
 const authMiddleware = (req,res,next) => {
     const auth = req.headers.authorization
     if (auth){
-        // let token = auth.split(' ')[1]
         let data = jwt.verify(auth,jwtSecret)
-        // console.log(data)
         req.userId = data.userId
         next()
     }else{
