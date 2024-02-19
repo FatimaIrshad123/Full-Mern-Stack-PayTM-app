2da17263d70aa2307748d5f1bbb822e8f649540b
Fatima Irshad <154042550+FatimaIrshad123@users.noreply.github.com>
2024-02-19 09:55:21 +0500

    Update index.js

--- backened/index.js
+++ backened/index.js
@@ -1,17 +1,11 @@
 const express = require('express')
 const cors = require('cors')
-
 const mainrouter = require('./routes/index')
 const userrouter = require('./routes/user')
 const app = express()
 const port = 3000
 
-
 app.use(cors())
 app.use(express.json())
-
 app.use('/api/r1',mainrouter)
-
-
-
-app.listen(port)
+app.listen(port => () => console.log(`App runing on port ${port}`))
