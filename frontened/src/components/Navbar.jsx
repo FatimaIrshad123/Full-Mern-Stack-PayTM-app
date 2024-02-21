b0ace7278c981e2ef90a05b60990a6b5a148ac2a
Fatima Irshad <154042550+FatimaIrshad123@users.noreply.github.com>
2024-02-21 18:36:20 +0500

    Update Navbar.jsx

--- frontened/src/components/Navbar.jsx
+++ frontened/src/components/Navbar.jsx
@@ -2,9 +2,6 @@ import { useEffect } from "react";
 import { Link } from "react-router-dom";
 
 export default function Navbar(){
-    // useEffect(() => {
-
-    
     if (!localStorage.getItem('token')){
     return(
         <div>
@@ -13,11 +10,13 @@ export default function Navbar(){
                     <h2 className="m-5"><Link to={'/'}>PayTm</Link></h2>
                 </div>
                 <div className="flex space-x-4 m-5 ">
-                <button className="p-3 px-7 rounded-lg bg-lime-500 text-white">Welcome</button>
+                    <button className="p-3 px-7 rounded-lg bg-lime-500 text-white">Welcome</button>
                     <button className="p-3 px-7 rounded-lg bg-lime-500 text-white">
-                        <Link to={'/signin'}>Login</Link></button>
+                        <Link to={'/signin'}>Login</Link>
+                    </button>
                     <button className="p-3 px-7 rounded-lg bg-lime-500 text-white">
-                        <Link to={'/signup'}>Sign up</Link></button>                   
+                        <Link to={'/signup'}>Sign up</Link>
+                    </button>                   
                 </div>
             </div>
         </div>
@@ -25,25 +24,26 @@ export default function Navbar(){
 }else {
     return(
         <div>
-            <div  className='flex justify-between border-2 rounded-lg shadow-lg m-2'>
+            <div className='flex justify-between border-2 rounded-lg shadow-lg m-2'>
                 <div>
                     <h2 className="m-3"><Link to={'/'}>PayTm</Link></h2>
                 </div>
                 <div className="flex space-x-4 m-3 ">  
                     <button className="p-3 px-7 rounded-lg bg-lime-500 text-white">
-                        <Link to={'/dashboard'}>Dashboard</Link></button>
+                        <Link to={'/dashboard'}>Dashboard</Link>
+                    </button>
                     <button className="p-3 px-7 rounded-lg bg-lime-500 text-white">
-                        <Link to={'/sendmoney'}>Transaction</Link></button>
+                        <Link to={'/sendmoney'}>Transaction</Link>
+                    </button>
                     <button className="p-3 px-7 rounded-lg bg-lime-500 text-white" 
-                    onClick={() => {
-                        localStorage.clear()
-                    }}>
-                        <Link to={'/signin'}>Logout</Link></button>
+                        onClick={() => {
+                            localStorage.clear()
+                        }}>
+                        <Link to={'/signin'}>Logout</Link>
+                    </button>
                 </div>
             </div>
         </div>
     )
-
-    }
-// },[])
+  }
 }
